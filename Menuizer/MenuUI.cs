f23dc0e7435a17d28d79ea8cd0ab0e2d2881a5eb
Wictchen <153632598+wictchen@users.noreply.github.com>
2024-01-08 15:01:39 -0300

    feat: add class to draw a simple menu

---
+++ Menuizer/MenuUI.cs
@@ -0,0 +1,37 @@
+﻿namespace Menuizer;
+
+public class MenuUI
+{
+    private static string[]? _items { get; set; }
+    private static int _selectedOption { get; set; }
+
+    static MenuUI()
+    {
+        Console.CursorVisible = false;
+    }
+
+    public static void DrawMenu(params string[] items)
+    {
+        _items = items;
+
+        while (true)
+        {
+            Console.Clear();
+
+            foreach (var i in _items ?? Array.Empty<string>())
+            {
+                if (i == _items?.ElementAt(_selectedOption))
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine(new string(' ', 20) + i);
+
+                Console.ResetColor();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Menuizer/Program.cs
@@ -0,0 +1,3 @@
+﻿using Menuizer;
+
+MenuUI.DrawMenu("Item 1", "Item 2", "Item 3");
