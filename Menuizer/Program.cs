7b0df0ce24e0ab9423d018694436e79a0638fd75
Wictchen <153632598+wictchen@users.noreply.github.com>
2024-01-18 14:42:23 -0300

    refactor: extract main menu code from Program to Screens class

--- Menuizer/Program.cs
+++ Menuizer/Program.cs
@@ -1,10 +1,3 @@
 ﻿using Menuizer;
 
-MainMenu();
-
-static void MainMenu()
-{
-    MenuUI.HeaderMessage = "Main Menu";
-    MenuUI.Message = "Your option:";
-    MenuUI.DrawMenu("Item 1", "Item 2", "Item 3");
-}
+Screens.MainMenu();
---
+++ Menuizer/Screens.cs
@@ -0,0 +1,12 @@
+﻿namespace Menuizer;
+
+public class Screens
+{
+    public static void MainMenu()
+    {
+        Console.Title = "Menuizer";
+        MenuUI.HeaderMessage = "Main Menu";
+        MenuUI.Message = "Your option:";
+        MenuUI.DrawMenu("Item 1", "Item 2", "Item 3");
+    }
+}
