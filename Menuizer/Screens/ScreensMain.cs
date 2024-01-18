ec7e8d398933e77981ad9dd0540bee05b4e2cea0
Wictchen <153632598+wictchen@users.noreply.github.com>
2024-01-18 15:48:13 -0300

    refactor: organize menu methods into separate files

--- Menuizer/Screens/ScreensMain.cs
+++ Menuizer/Screens/ScreensMain.cs
@@ -1,12 +1,17 @@
 ﻿namespace Menuizer;
 
-public class Screens
+public partial class Screens
 {
     public static void MainMenu()
     {
         Console.Title = "Menuizer";
         MenuUI.HeaderMessage = "Main Menu";
-        MenuUI.Message = "Your option:";
         MenuUI.DrawMenu("Item 1", "Item 2", "Item 3");
+        switch (MenuUI.SelectedOption)
+        {
+            case 0: Screen1(); break;
+            case 1: Screen2(); break;
+            case 2: Screen3(); break;
+        }
     }
 }
---
+++ Menuizer/Screens/ScreensSub.cs
@@ -0,0 +1,28 @@
+﻿namespace Menuizer;
+
+public partial class Screens
+{
+    private static void Screen1()
+    {
+        MenuUI.HeaderMessage = "Screen 1";
+        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
+        if (MenuUI.SelectedOption == 2)
+            MainMenu();
+    }
+
+    private static void Screen2()
+    {
+        MenuUI.HeaderMessage = "Screen 2";
+        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
+        if (MenuUI.SelectedOption == 2)
+            MainMenu();
+    }
+
+    private static void Screen3()
+    {
+        MenuUI.HeaderMessage = "Screen 3";
+        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
+        if (MenuUI.SelectedOption == 2)
+            MainMenu();
+    }
+}
