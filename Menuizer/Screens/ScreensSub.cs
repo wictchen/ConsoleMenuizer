fc9fb1137aab5fd19a5a9693798011fafc7512d4
Wictchen <153632598+wictchen@users.noreply.github.com>
2024-01-19 12:03:08 -0300

    feat: add confirmation screen in Screens class

--- Menuizer/Screens/ScreensSub.cs
+++ Menuizer/Screens/ScreensSub.cs
@@ -5,24 +5,39 @@ public partial class Screens
     private static void Screen1()
     {
         MenuUI.HeaderMessage = "Screen 1";
-        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
-        if (MenuUI.SelectedOption == 2)
-            MainMenu();
+        MenuUI.DrawMenu("Item 1", "Item 2", "Item 3", "Back");
+        switch (MenuUI.SelectedOption)
+        {
+            case 0: Confirm(); break;
+            case 1: Confirm(); break;
+            case 2: Confirm(); break;
+            case 3: MainMenu(); break;
+        }
     }
 
     private static void Screen2()
     {
         MenuUI.HeaderMessage = "Screen 2";
-        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
-        if (MenuUI.SelectedOption == 2)
-            MainMenu();
+        MenuUI.DrawMenu("Item 1", "Item 2", "Item 3", "Back");
+        switch (MenuUI.SelectedOption)
+        {
+            case 0: Confirm(); break;
+            case 1: Confirm(); break;
+            case 2: Confirm(); break;
+            case 3: MainMenu(); break;
+        }
     }
 
     private static void Screen3()
     {
         MenuUI.HeaderMessage = "Screen 3";
-        MenuUI.DrawMenu("Item 1", "Item 2", "Back");
-        if (MenuUI.SelectedOption == 2)
-            MainMenu();
+        MenuUI.DrawMenu("Item 1", "Item 2", "Item 3", "Back");
+        switch (MenuUI.SelectedOption)
+        {
+            case 0: Confirm(); break;
+            case 1: Confirm(); break;
+            case 2: Confirm(); break;
+            case 3: MainMenu(); break;
+        }
     }
 }
